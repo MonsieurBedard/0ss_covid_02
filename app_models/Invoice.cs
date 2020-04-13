5e2fb33c3c313ef1a366e67bd0f0037ecd76b6bc
Alexandre Bédard <alexandrebedard.xyz>
2020-04-13 14:41:01 -0400

    3.2.1

---
+++ app_models/Invoice.cs
@@ -0,0 +1,51 @@
+﻿using app_models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace BillingManagement.Models
+{
+    public class Invoice : INotifyPropertyChanged
+    {
+        static int nextId;
+        public int InvoiceId { get; private set; }
+        public DateTime CreationDateTime { get; private set; }
+
+        private Customer _customer;
+        public Customer Customer
+        {
+            get { return _customer; }
+            set
+            {
+                _customer = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double _subTotal;
+        public double SubTotal {
+            get { return _subTotal; } 
+            set
+            {
+                _subTotal = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(FedTax));
+                OnPropertyChanged(nameof(ProvTax));
+                OnPropertyChanged(nameof(Total));
+            }
+        }
+
+        public double FedTax => SubTotal * 0.05;
+        public double ProvTax => SubTotal * 0.09975;
+        public double Total => SubTotal + FedTax + ProvTax;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
