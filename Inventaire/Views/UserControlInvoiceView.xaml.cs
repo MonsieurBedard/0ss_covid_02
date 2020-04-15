0b4dc75c6db6f4ddf571515d872916d003e24481
Alexandre Bédard <alexandrebedard.xyz>
2020-04-15 16:40:07 -0400

    3.5.1

---
+++ Inventaire/Views/UserControlInvoiceView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using BillingManagement.UI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BillingManagement.UI.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour UserControlInvoiceView.xaml
+    /// </summary>
+    public partial class UserControlInvoiceView : UserControl
+    {
+        InvoiceViewModel _vm;
+
+        public UserControlInvoiceView(InvoiceViewModel vm)
+        {
+            InitializeComponent();
+
+            _vm = vm;
+            DataContext = _vm;
+        }
+    }
+}
